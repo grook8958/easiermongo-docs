62d864b001b787bf95d81f117bd93090404b05af
grook8958 <78109323+grook8958@users.noreply.github.com>
2022-06-08 23:06:01 +0200

    Added docs search bar

---
+++ scripts/searchBar.js
@@ -0,0 +1,54 @@
+const classesContainer = document.querySelector("[data-docs-classes-container]")
+const typedefsContainer = document.querySelector("[data-docs-typedefs-container]")
+const searchInput = document.querySelector("[data-search]")
+
+//{name: 'string', href: 'link'}
+import rawDocs from '../assets/docs_v1.json' assert {type: "json"};
+
+let classes = [];
+let typedefs = [];
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase();
+  classes.forEach(docs => {
+    const isVisible =
+      docs.name.toLowerCase().includes(value) ||
+      docs.href.toLowerCase().includes(value) ||
+      docs.fx.includes(value);
+    console.log(isVisible)
+    if (!isVisible) docs.element.setAttribute("class", "hide");
+    else if (isVisible) docs.element.removeAttribute("class", "hide");
+  });
+
+  typedefs.forEach(docs => {
+    const isVisible =
+      docs.name.toLowerCase().includes(value) ||
+      docs.href.toLowerCase().includes(value) ||
+      docs.fx.includes(value);
+    console.log(isVisible)
+    if (!isVisible) docs.element.setAttribute("class", "hide");
+    else if (isVisible) docs.element.removeAttribute("class", "hide");
+  });
+});
+
+window.addEventListener('load', () => {
+    rawDocs.classes.forEach(doc => {
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.setAttribute('href', doc.href);
+        link.textContent = doc.name;
+        li.append(link);
+        classesContainer.append(li);
+        classes.push({ name: doc.name, href: doc.href, element: li })
+    });
+
+    rawDocs.typedefs.forEach(doc => {
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.setAttribute('href', doc.href);
+        link.textContent = doc.name;
+        li.append(link);
+        typedefsContainer.append(li);
+        typedefs.push({ name: doc.name, href: doc.href, element: li })
+    });
+});
