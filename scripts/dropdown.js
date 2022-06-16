ceeff0aa0c3d27751c17f7429a89ca9eb4491c11
grook8958 <78109323+grook8958@users.noreply.github.com>
2022-06-16 17:17:35 +0200

    Added the documentation for the MongoClient

---
+++ scripts/dropdown.js
@@ -0,0 +1,28 @@
+const propertiesDropdownBtn = document.getElementById('properties-dropdown-btn');
+const methodsDropdownBtn = document.getElementById('methods-dropdown-btn');
+const eventsDropdownBtn = document.getElementById('events-dropdown-btn');
+
+const propertiesDropdownList = document.getElementById('properties-dropdown-list');
+const methodsDropdownList = document.getElementById('methods-dropdown-list');
+const eventsDropdownList = document.getElementById('events-dropdown-list');
+
+propertiesDropdownBtn.addEventListener('click', () => toggleDropdown(propertiesDropdownList));
+methodsDropdownBtn.addEventListener('click', () => toggleDropdown(methodsDropdownList));
+eventsDropdownBtn.addEventListener('click', () => toggleDropdown(eventsDropdownList));
+
+function toggleDropdown(dropdown) {
+    const type = dropdown.id.replace('-list', '')
+    const arrowUp = document.getElementById(`${type}-btn__icon__hide`);
+    const arrowDown = document.getElementById(`${type}-btn__icon__show`);
+    if (arrowUp && arrowDown && arrowDown.hasAttribute('hide') && arrowDown.attributes.getNamedItem('hide').value == "true") {
+        arrowDown.removeAttribute('hide')
+        arrowUp.setAttribute('hide', 'true')
+        dropdown.removeAttribute('hide')
+    } else {
+        arrowDown.setAttribute('hide', 'true')
+        arrowUp.removeAttribute('hide')
+        dropdown.setAttribute('hide', 'true')
+    }
+}
+
+//m19 9l-7 7l-7-7
