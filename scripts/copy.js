812a6879d20bf969865a96ab13739c593bbba5b7
grook8958 <78109323+grook8958@users.noreply.github.com>
2022-10-06 16:13:28 +0200

    Fixed indentation.

--- scripts/copy.js
+++ scripts/copy.js
@@ -14,4 +14,4 @@ export const copy = (tippy) => {
             document.getElementById("copy-button1__icon__path").setAttribute("d", "M8 5H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-1M8 5a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2M8 5a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2m0 0h2a2 2 0 0 1 2 2v3m2 4H10m0 0l3-3m-3 3l3 3");
         }, 3000);
     })
-    }
+}
