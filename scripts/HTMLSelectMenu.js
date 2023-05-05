de3c4cab93e26c875166fb2b8191b989f0e12fc8
grook8958 <clement.puks@gmail.com>
2023-05-06 00:03:55 +0200

    (fix): Opening the select menu reseting to its default value.
    - (feat): Added the option to change to `facingUpArrowData`, `facingDownArrowData`, `skipValidation`, `selectMenuButtonSelector`, `selectMenuPathSelectore`
    - (style): Added a HTML code snippet for proper use of the class.
    - (fix): Fixed missing name for `onSelected` in the contructor's JSDOC.
    - (refactor): Added  `facingUpArrowData` & `facingDownArrowData variables
    - (refactor): Renamed `_data` to `data`

--- scripts/HTMLSelectMenu.js
+++ scripts/HTMLSelectMenu.js
@@ -1,21 +1,42 @@
+/**
+ * For the select menu to properly work, it must be organized in the following way:
+ *  <div id="[select-menu-id]" class="select-menu" style="width:100px;" value="">
+ *      <button class="select-menu-button" type="button">
+ *          <span class="selected">v2.0</span>
+ *          <svg class="select-menu-arrow" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24" width="1.2em" height="1.2em"><path class="select-menu-arrow-path" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5 15l7-7l7 7"></path></svg>
+ *      </button>
+ *  </div>
+ */
+/**
+ * HTMLSelectMenu Handler
+ */
 class HTMLSelectMenu {
+    /**
+     * @typedef {Object} HTMLSelectMenuOptions
+     * @property {?string} facingUpArrowData
+     * @property {?string} facingDownArrowData
+     * @property {?boolean} skipValidation
+     * @property {?string} selectMenuButtonSelector
+     * @property {?string} selectMenuArrowPathSelector
+     */
     /**
      * A custom HTML Select Menu.
      * @param {string} containerId 
      * @param {Array<string>} data 
-     * @param {(item: HTMLElement) => {}}
+     * @param {(item: HTMLElement) => {}} onSelected
+     * @param {HTMLSelectMenuOptions} options
      */
-    constructor(containerId, data, onSelected = (item) => { return undefined }) {
+    constructor(containerId, data, onSelected = (item) => { return undefined }, options = {}) {
         /**
          * Raw arguments passed into the constructor.
          */
-        this.rawArgs = { containerId, data, onSelected };
-
+        this.rawArgs = { containerId, data, onSelected, options };
+    
         /**
          * The container 
          */
         this.container = document.getElementById(containerId);
-
+        
         /**
          * @typedef {Object} HTMLSelectMenuData
          * @property {string} selected The current selected item
@@ -36,12 +57,27 @@ class HTMLSelectMenu {
 
         this.childElements = {
             itemContainer: null,
-            button: this.container.querySelector('.select-menu-button'),
-            arrowPath: this.container.querySelector('.select-menu-arrow-path'),
+            button: this.container.querySelector(options.selectMenuButtonSelector ?? '.select-menu-button'),
+            arrowPath: this.container.querySelector(options.selectMenuButtonSelector ?? '.select-menu-arrow-path'),
         }
 
+        /**
+         * Data code for the up arrow symbol.
+         * @type {string}
+         * @readonly
+         */
+        this.facingUpArrowData = options.facingUpArrowData ?? 'm5 15l7-7l7 7';
+
+        /**
+         * Data code for the down arrow symbol.
+         * @type {string}
+         * @readonly
+         */
+        this.facingDownArrowData = options.facingUpArrowData ?? 'm19 9l-7 7l-7-7';
+
+
         //Validate the options
-        this._validate();
+        this._validate(options.skipValidation ?? false);
 
         //Initialize the function
         this.init();
@@ -84,11 +120,10 @@ class HTMLSelectMenu {
      * @param {HTMLElement} element 
      */
     handleClick(element) {
-        this.data.current = element.innerHTML;
+        this.data.selected = element.innerHTML;
         this.closeSelectMenu();
         this._toggleArrow();
-        this.setSelected(this.data.current);
-
+        this.setSelected(this.data.selected);
         this.onSelected(element);
     }
 
@@ -119,10 +154,8 @@ class HTMLSelectMenu {
      * @returns 
      */
     getState(arrowPathElement = this.container.querySelector('.select-menu-arrow-path')) {
-        const facingUpArrowData = 'm5 15l7-7l7 7';
-        const facingDownArrowData = 'm19 9l-7 7l-7-7';
-        if (arrowPathElement.getAttribute('d') === facingDownArrowData) return 1;
-        else if (arrowPathElement.getAttribute('d') === facingUpArrowData) return 0;
+        if (arrowPathElement.getAttribute('d') === this.facingDownArrowData) return 1;
+        else if (arrowPathElement.getAttribute('d') === this.facingUpArrowData) return 0;
     }
 
     /**
@@ -130,10 +163,8 @@ class HTMLSelectMenu {
      * @param {HTMLElement} arrowPathElement 
      */
     _toggleArrow(arrowPathElement = this.container.querySelector('.select-menu-arrow-path')) {
-        const facingUpArrowData = 'm5 15l7-7l7 7';
-        const facingDownArrowData = 'm19 9l-7 7l-7-7';
-        if (arrowPathElement.getAttribute('d') === facingDownArrowData) arrowPathElement.setAttribute('d', facingUpArrowData);
-        else if (arrowPathElement.getAttribute('d') === facingUpArrowData) arrowPathElement.setAttribute('d', facingDownArrowData);
+        if (arrowPathElement.getAttribute('d') === this.facingDownArrowData) arrowPathElement.setAttribute('d', this.facingUpArrowData);
+        else if (arrowPathElement.getAttribute('d') === this.facingUpArrowData) arrowPathElement.setAttribute('d', this.facingDownArrowData);
     }
 
     /**
@@ -141,14 +172,15 @@ class HTMLSelectMenu {
      * @param {Array<string>} data 
      * @returns 
      */
-    _parseData(data = this._data) {
+    _parseData(data = this.data) {
         return { selected: data[0], stored: data};
     }
 
     /**
      * Validate the options.
      */
-    _validate() {
+    _validate(skipValidation = false) {
+        if (skipValidation) return;
         if (typeof this.rawArgs.onSelected !== 'function') throw new TypeError('[INVALID_TYPE] The "onSelected" option must be a function.');
         if (typeof this.rawArgs.containerId !== 'string') throw new TypeError('[INVALID_TYPE] The "containerId" option must be a string.');
         if (!Array.isArray(this.rawArgs.data)) throw new TypeError('[INVALID_TYPE] The "data" option must be an Array of strings.');
